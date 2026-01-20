ebe0be6243572d20da9071cfbd35de5295dbf949
ema-vibcod-dotcom <ema.752ea@gmail.com>
2026-01-20 16:30:33 -0500

    Ajuste de rutas para PythonAnywhere

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev_secret_key_change_in_prod'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///coworking.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'instance', 'coworking.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
